7ef5299481d309542d28797b3a54beeeb5510395
skeptrune <skeptrune@pop-os.localdomain>
2023-01-27 18:39:08 -0600

    complete basic styling for arguflow

--- src/root.tsx
+++ src/root.tsx
@@ -24,23 +24,13 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - With TailwindCSS</Title>
+        <Title>arguflow app</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <nav class="bg-sky-800">
-              <ul class="container flex items-center p-3 text-gray-200">
-                <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-                  <A href="/">Home</A>
-                </li>
-                <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-                  <A href="/about">About</A>
-                </li>
-              </ul>
-            </nav>
             <Routes>
               <FileRoutes />
             </Routes>
