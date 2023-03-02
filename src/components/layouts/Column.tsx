f1ab9804c5e694a1e8be4df80180e7efe2f9e477
Nicholas Khami <nicholas.khami@gmail.com>
2023-03-02 09:54:24 -0600

    bugfix: change column open to click instead of hover

--- src/components/layouts/Column.tsx
+++ src/components/layouts/Column.tsx
@@ -4,28 +4,17 @@ export interface ColumnProps {
   children: JSX.Element | JSX.Element[] | undefined | null;
   classList: Record<string, boolean | undefined> | undefined;
   visible: boolean;
-  onMouseEnter?: () => void;
-  onMouseLeave?: () => void;
+  onClosedClick?: () => void;
 }
 
 export const Column = (props: ColumnProps) => {
-  const onMouseEnterFunction = () => {
-    props.onMouseEnter?.();
-  };
-  const onMouseLeaveFunction = () => {
-    props.onMouseLeave?.();
+  const onCloseClickFunction = () => {
+    if (props.visible) return;
+    props.onClosedClick?.();
   };
 
   return (
-    <div
-      onMouseEnter={() => {
-        onMouseEnterFunction();
-      }}
-      onMouseLeave={() => {
-        onMouseLeaveFunction();
-      }}
-      classList={props.classList}
-    >
+    <div onClick={onCloseClickFunction} classList={props.classList}>
       <div class="overflow-y-auto p-3 scrollbar-thin scrollbar-track-gray-900 scrollbar-thumb-gray-700">
         {props.visible && props.children}
       </div>
