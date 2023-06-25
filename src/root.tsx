946c38aeafb437b82cc17babc2a20cd63eae25e8
Denzell Ford <fdenzell@gmail.com>
2023-06-25 00:35:23 -0500

    bugfix: found stupid workaround for solid-start

--- src/root.tsx
+++ src/root.tsx
@@ -13,6 +13,7 @@ import {
 import "./root.css";
 import RelayStoreContext from "./contexts/GlobalContext";
 import theme from "./contexts/GlobalContext";
+import { createEffect, onCleanup } from "solid-js";
 
 export default function Root() {
   let setDark = "";
@@ -22,17 +23,23 @@ export default function Root() {
       window.matchMedia("(prefers-color-scheme: dark)").matches)
   )
     setDark = "dark";
+
+  createEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://perhaps.arguflow.com/js/script.js";
+    script.defer = true;
+    script["data-domain"] = "app.arguflow.gg";
+    document.body.appendChild(script);
+
+    onCleanup(() => document.body.removeChild(script));
+  });
+
   return (
     <Html lang="en" class={setDark}>
       <Head>
         <Title>arguflow app</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script
-          defer
-          data-domain="app.arguflow.gg"
-          src="https://perhaps.arguflow.com/js/script.js"
-        />
       </Head>
       <Body class="bg-white overflow-x-hidden scrollbar-thin scrollbar-track-gray-900 scrollbar-thumb-gray-700">
         <ErrorBoundary>
